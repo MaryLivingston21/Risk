6ee47e5a50d2182e5ef1669b5f656208c0d2b775
marylivingston21 <mliving1@uvm.edu>
2020-02-24 12:47:10 -0500

    initializing classes

--- Deck.py
+++ Deck.py
@@ -5,3 +5,10 @@ class DECK:
 
     def __init__(self):
         self.cards = []
+
+    def addCard(self, card):
+        self.cards.append(card)
+
+    ##remove card from deck
+    def removeCard(self, card):
+        self.cards.remove(card)
--- Risk.py
+++ Risk.py
@@ -7,9 +7,12 @@ class RISK:
     def __init__(self, users):
         ## might have to initialize users, depends on interface
         self.users = users
+        self.numSetsTurnedIn = 0
+        self.userTurn = ""
+        self.stage = ""
 
     def boardSetUp(self):
-        ##something
+        deck = DECK()
 
     def draft(self, user):
         ##something
@@ -19,3 +22,6 @@ class RISK:
 
     def fortify(self, user):
         ##something
+
+    def getCards(self, user):
+        ##add card to user Deck iff they won an attack
