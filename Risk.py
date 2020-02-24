233f67c75696c70776cbf83084dbc2f8a2e84eb0
marylivingston21 <mliving1@uvm.edu>
2020-02-24 12:40:28 -0500

    initializing classes

--- Risk.py
+++ Risk.py
@@ -5,4 +5,17 @@ from User import USER
 class RISK:
 
     def __init__(self, users):
+        ## might have to initialize users, depends on interface
         self.users = users
+
+    def boardSetUp(self):
+        ##something
+
+    def draft(self, user):
+        ##something
+
+    def attack(self, user):
+        ##something
+
+    def fortify(self, user):
+        ##something
